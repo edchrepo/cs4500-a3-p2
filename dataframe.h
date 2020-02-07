6fe1ddb8a7074375d55ca6bed73676ba420a1dd1
Andrew Hurd <Hurda21@gmail.com>
2020-02-07 17:32:06 -0500

    changed parameter names for dataframe class

--- dataframe.h
+++ dataframe.h
@@ -13,7 +13,7 @@ public:
 	}
 
 	// Constructor for passing in specified columns.
-	DataFrame(Column* c, ...) {
+	DataFrame(Column* col, ...) {
 	}
 
 	// Destructor.
@@ -25,24 +25,32 @@ public:
 	}
 
 	// Adds a new column to the DataFrame.
-	void add(Column* c) {
+	void add(Column* col) {
 	}
 
 	// Adds a list of columns to the DataFrame.
-	void addAll(Column** cc) {
+	void add_all(Column** cols) {
 	}
 
 	// Adds a row (entry) to the end of each column of the DataFrame.
 	// The parameters passed in must match the type of each column it's added to.
-	void addRow(Object* o, ...) {
+	void add_row(Object* o, ...) {
 	}
 
 	// Gets the object at the specified row and specified column.
-	Object* get(size_t row, size_t column) {
+	Object* get(size_t r, size_t c) {
 	}
 
 	// Gets the object at the specified row name and specified column name.
-	Object* get(const char* row_name, const char* column_name) {
+	Object* get(const char* rname, const char* cname) {
+	}
+
+	// Gets the name of the given row index.
+	char* row_name(size_t r) {
+	}
+	
+	// Gets the name of the given column index.
+	char* col_name(size_t c) {
 	}
 
 	// Gets the data row count
