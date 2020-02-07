975d3875a56cc9f45a5d64bae18af76851e71f93
Edward Chung <EdwardC@Edwards-MacBook-Pro.local>
2020-02-07 07:44:09 -0500

    adding cellobject

---
+++ cellobject.h
@@ -0,0 +1,30 @@
+//lang::CwC
+#pragma once
+
+#include <cstdlib>
+
+/**
+ * A class that represents the top of the object hierarchy.
+   Altered from chasebish's Object interface.
+ * author: chasebish */
+class CellObject {
+public:
+
+  CellObject() {
+  }
+
+  /* Default Object destructor, to be overriden by subclasses */
+  virtual ~CellObject() { }
+
+
+  /** VIRTUAL METHODS **/
+
+  /* Returns whether two objects are equal, to be overriden by subclasses */
+  virtual bool equals(Object* const obj) {
+  }
+
+  /* Returns an object's hash value. Identical objects should have identical hashes */
+  virtual size_t hash() {
+  }
+
+};
